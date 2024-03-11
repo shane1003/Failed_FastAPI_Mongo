29798dfa038775cee85ab73c368b488347527603
shane1003 <thyun.eom@gmail.com>
2024-03-11 22:26:37 +0900

    app.server X server.

--- app/server/app.py
+++ app/server/app.py
@@ -5,8 +5,13 @@ Tags are identifiers used to group routes.
 
 from fastapi import FastAPI
 
+from server.routes.student import router as StudentRouter
+
 app = FastAPI()
+
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
+
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"Message" : "Welcome to this fantastic app!"}
+    return {"message": "Welcome to this fantastic app!"}
